cbc1ca7b37ed2de071c5e8b5d36a9cafdee197c9
ernesto <ernestoponce27@gmail.com>
2024-03-17 02:34:45 -0300

    send email - update response

--- send-email/dagger/main.go
+++ send-email/dagger/main.go
@@ -1,22 +1,8 @@
-// A generated module for SendEmail functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
+// This module send a email using gomail package
 
 package main
 
 import (
-	"fmt"
-
 	"gopkg.in/gomail.v2"
 )
 
@@ -32,7 +18,7 @@ func (m *SendEmail) SendEmail(
 	username string,
 	password string,
 
-) string {
+) (string, error) {
 
 	mail := gomail.NewMessage()
 	mail.SetHeader("From", from)
@@ -43,10 +29,9 @@ func (m *SendEmail) SendEmail(
 	d := gomail.NewDialer(host, port, username, password)
 
 	if err := d.DialAndSend(mail); err != nil {
-		fmt.Println(err)
-		return err.Error()
+		return "", err
 	}
 
-	return ""
+	return "Email sent successfully", nil
 
 }
