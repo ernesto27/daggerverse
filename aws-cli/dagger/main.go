8d79c08cdf898858335bfdee05f6cd02757c340c
Ernesto ponce <ernestoponce27@gmail.com>
2024-04-13 12:33:11 -0300

    add arg to mount files

--- aws-cli/dagger/main.go
+++ aws-cli/dagger/main.go
@@ -21,12 +21,39 @@ import (
 
 type AwsCli struct{}
 
-func (m *AwsCli) Command(args string, dirConfig *Directory) (string, error) {
-	r, err := dag.Container().
+// Example usage
+//
+// dagger call -m github.com/ernesto27/daggerverse/aws-cli run
+//     --command="s3 ls"  \
+//     --dir-config ~/.aws/
+
+func (m *AwsCli) Run(
+	// AWS CLI command to execute
+	command string,
+	// AWS config credentials directory
+	dirConfig *Directory,
+	// Directory with files to use in the command
+	// +optional
+	dirFiles *Directory) (string, error) {
+
+	commandToExecute := []string{}
+
+	for _, value := range strings.Split(command, " ") {
+		if value != "" {
+			commandToExecute = append(commandToExecute, value)
+		}
+	}
+
+	container := dag.Container().
 		From("amazon/aws-cli:latest").
-		WithMountedDirectory("/root/.aws/", dirConfig).
-		WithExec(strings.Split(args, " ")).
+		WithMountedDirectory("/root/.aws/", dirConfig)
+
+	if dirFiles != nil {
+		container = container.WithMountedDirectory("/aws/", dirFiles)
+	}
+
+	resp, err := container.WithExec(commandToExecute).
 		Stdout(context.Background())
 
-	return r, err
+	return resp, err
 }
