e6d73b6f68ea48712ea9b0ca1fb9d0da03c9ce7d
Ernesto ponce <ernestoponce27@gmail.com>
2024-04-19 22:18:18 -0300

    update charm freeze

--- charm/dagger/main.go
+++ charm/dagger/main.go
@@ -1,33 +1,31 @@
 // A generated module for Charm functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
 
 package main
 
 import (
 	"context"
+	"strings"
 )
 
 type Charm struct{}
 
-func (c *Charm) Vhs(ctx context.Context, fileTape *File) *File {
+// Executes a charm VHS command that generates a gif from a demo.tape file
+func (c *Charm) Vhs(
+	ctx context.Context,
+	// tape file to generate gif
+	fileTape *File,
+) *File {
 	return c.baseVhs(fileTape).
 		WithMountedFile("demo.tape", fileTape).
 		WithExec([]string{"demo.tape", "-o", "output.gif"}).
 		File("output.gif")
 }
 
-func (c *Charm) VhsPublish(ctx context.Context, file *File) (string, error) {
+// Publish a gif file to the VHS service
+func (c *Charm) VhsPublish(
+	ctx context.Context,
+	file *File,
+) (string, error) {
 	return c.baseVhs(file).
 		WithMountedFile("demo.gif", file).
 		WithExec([]string{"publish", "demo.gif"}).
@@ -39,15 +37,36 @@ func (c *Charm) baseVhs(file *File) *Container {
 		From("ghcr.io/charmbracelet/vhs")
 }
 
-func (c *Charm) Freeze(ctx context.Context) (string, error) {
+// Executes a charm freeze command that generate images from a source code file
+func (c *Charm) Freeze(
+	ctx context.Context,
+	// Source code file to generate images
+	file *File,
+	// +optional
+	params string,
+) *File {
+	fileName, _ := file.Name(ctx)
+
+	nameFileExport := "freeze.png"
+	paramsToAdd := []string{}
+
+	paramsData := strings.Split(params, " ")
+	for index, value := range paramsData {
+		if value != "" {
+			if value == "-o" || value == "--output" {
+				nameFileExport = paramsData[index+1]
+			}
+			paramsToAdd = append(paramsToAdd, value)
+		}
+	}
+
 	return dag.Container().
 		From("alpine:latest").
+		WithMountedFile(fileName, file).
 		WithExec([]string{"apk", "update"}).
 		WithExec([]string{"apk", "add", "wget"}).
 		WithExec([]string{"wget", "https://github.com/charmbracelet/freeze/releases/download/v0.1.6/freeze_0.1.6_Linux_x86_64.tar.gz"}).
-		WithExec([]string{"mkdir", "freeze"}).
-		// WithExec([]string{"tar", "-xvf", "freeze_0.1.6_Linux_x86_64.tar.gz", "--directory", "freeze"}).
 		WithExec([]string{"tar", "-xvf", "freeze_0.1.6_Linux_x86_64.tar.gz"}).
-		WithExec([]string{"freeze_0.1.6_Linux_x86_64/freeze"}).
-		Stdout(ctx)
+		WithExec(append([]string{"freeze_0.1.6_Linux_x86_64/freeze", fileName}, paramsToAdd...)).
+		File(nameFileExport)
 }
